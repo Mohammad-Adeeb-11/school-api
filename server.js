6a104323c444fabc0c555e6853bf4ab7b5f7baca
Mohammad Adeeb <mdadeeb7866@gmail.com>
2026-03-07 01:59:14 +0530

    Added environment variables

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -9,10 +11,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Routes
 app.use("/", schoolRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
